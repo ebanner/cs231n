ae42857b95d7b0b880a28e1dc9645283987ea475
Edward Banner <edward.banner@gmail.com>
2015-08-13 10:52:51 -0500

    Add vectorized svm gradient computation
    Such an awesome exercise in vecotrization. I learned about multiplying matricies
    with outer products!

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -23,12 +23,22 @@ def svm_loss_naive(W, X, y, reg):
   for i in xrange(num_train):
     scores = W.dot(X[:, i])
     correct_class_score = scores[y[i]]
+    
+    num_contrib_classes = 0
     for j in xrange(num_classes):
       if j == y[i]:
         continue
+        
       margin = scores[j] - correct_class_score + 1 # note delta = 1
+    
       if margin > 0:
         loss += margin
+        # Update dW_incorrect_class weights
+        dW[j] += X[:, i]
+        num_contrib_classes += 1
+        
+    # Update dW_correct_class weights
+    dW[y[i]] -= num_contrib_classes * X[:, i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
@@ -46,6 +56,7 @@ def svm_loss_naive(W, X, y, reg):
   # code above to compute the gradient.                                       #
   #############################################################################
 
+  dW /= num_train
 
   return loss, dW
 
@@ -59,12 +70,27 @@ def svm_loss_vectorized(W, X, y, reg):
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
 
+  # Get dimensions (great way to foreshadow impending matrix multiplication!)
+  (num_class, D), (D, num_train) = (W.shape, X.shape)
+
   #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  # Compute class scores
+  scores = np.dot(W, X)
+
+  # Get the correct class scores and compute losses from it
+  correct_class_scores = scores[y, range(num_train)]
+  losses = scores - correct_class_scores + 1
+
+  # Hinge
+  losses[losses < 0] = 0
+
+  # Zero out the losses in which y was the correct class and add up the losses
+  losses[y, range(num_train)] = 0
+  loss = losses.sum() / num_train
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -79,7 +105,17 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+  # Make `losses` into a bit matrix so we know which classes in which examples contribute to gradient
+  losses[losses > 0] = 1
+  
+  # Put in the number of bad class scores for each example and this becomes the term of each correct class' gradient
+  losses[y, range(num_train)] = -losses.sum(axis=0)
+
+  # Take the sum of outer products of the bit matrix and X itself
+  #
+  # There's no way I would have found this out by myself. Credit to https://github.com/MyHumbleSelf/cs231n/blob/master/assignment1/cs231n/classifiers/linear_svm.py. Thank you for putting this up so I could study from it and learn an awesome trick!
+  dW = np.dot(losses, X.T)
+  dW /= num_train
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
